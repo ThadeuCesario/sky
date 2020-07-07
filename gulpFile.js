1581ba3546851151fda5d28d2655c1bc662b2533
Thadeu Cesário <thadeu.cesario@icloud.com>
2020-07-07 19:52:24 -0300

    initial project config

---
+++ gulpFile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+var sass = require('gulp-sass'); 
+
+var mainScssFile = 'public/src/scss/main-style.scss';
+var scssFiles = 'public/src/scss/**/*.scss';
+
+var sassProdOptions = {
+    outputStyle: 'compressed'
+}
+
+gulp.task('sassprod', async function(){
+    return gulp.src(mainScssFile)
+        .pipe(sass(sassProdOptions).on('error', sass.logError))
+        .pipe(rename('style-main.min.css'))
+        .pipe(gulp.dest('public/css'))
+});
+
+gulp.task('watch', function (done) {
+    gulp.watch(scssFiles, gulp.series('sassprod'));
+    done();
+});
+
+gulp.task('default', gulp.series('sassprod','watch'));

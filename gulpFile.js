44f53a3684c1ab275c3d8a992304c9c591d5e1d6
Thadeu Cesário <thadeu.cesario@icloud.com>
2020-07-07 22:44:21 -0300

    gulp settings - js and css

--- gulpFile.js
+++ gulpFile.js
@@ -1,9 +1,20 @@
 var gulp = require('gulp');
 var rename = require('gulp-rename');
 var sass = require('gulp-sass'); 
+var plumber  = require('gulp-plumber');
+var uglify   = require('gulp-terser');
+const babel = require('gulp-babel');
+var concat   = require('gulp-concat');
 
+//Develop Path
 var mainScssFile = 'src/scss/main-styles.scss';
 var scssFiles = 'src/scss/**/*.scss';
+var jsFiles = 'src/js/**/*.js';
+
+//Production Path
+var cssFolderProduction = 'public/css/';
+var jsFolderProduction = 'public/js/';
+var jsFolderProductionName = 'scripts.js';
 
 var sassProdOptions = {
     outputStyle: 'compressed'
@@ -13,12 +24,22 @@ gulp.task('sassprod', async function(){
     return gulp.src(mainScssFile)
         .pipe(sass(sassProdOptions).on('error', sass.logError))
         .pipe(rename('style-main.min.css'))
-        .pipe(gulp.dest('public/css'))
+        .pipe(gulp.dest(cssFolderProduction))
+});
+
+gulp.task('scripts', function(){
+    return gulp.src(jsFiles)
+            .pipe(concat(jsFolderProductionName))
+            .pipe(rename({suffix: '.min'}))
+            .pipe(uglify())
+            .pipe(gulp.dest(jsFolderProduction))
+
 });
 
 gulp.task('watch', function (done) {
     gulp.watch(scssFiles, gulp.series('sassprod'));
+    gulp.watch(jsFiles, gulp.series('scripts'));
     done();
 });
 
-gulp.task('default', gulp.series('sassprod','watch'));
+gulp.task('default', gulp.series('sassprod','scripts','watch'));
---
+++ src/js/teste.js
@@ -0,0 +1,5 @@
+function helloWorld(){
+  console.log('hello world ola ola!!!!!!!');
+}
+
+const abc = 1;

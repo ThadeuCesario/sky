657663c7606e442d0212d078b8a6de7d86914250
Thadeu Cesário <thadeu.cesario@icloud.com>
2020-07-09 01:58:51 -0300

    slider highlights

--- src/js/slider/slider.js
+++ src/js/slider/slider.js
@@ -1,3 +1,9 @@
 function sliderLoadHighlights(){
-  $(".sky-main__highlights.owl-carousel").owlCarousel();
+  $(".sky-main__highlights.owl-carousel").owlCarousel({
+    center: true,
+    items: 3,
+    loop: true,
+    margin: 15,
+    nav: true,
+  });
 }

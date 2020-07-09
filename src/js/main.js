40354d97e2886c951c37b443f40f10fa0df7c5a2
Thadeu Cesário <thadeu.cesario@icloud.com>
2020-07-09 17:44:57 -0300

    banner movies

--- src/js/main.js
+++ src/js/main.js
@@ -41,14 +41,16 @@ window.onload = function(){
 
       articlePortraitMovies.innerHTML = `
         <h2 class='sky-main__portrait-title'>${typesMoviesPortrait[i]}</h2>
-        <div class='sky-main__portrait-movie owl-carousel'>${moviesSpecificCategory.map(movie => `<img src='${(movie['images'][0]['url'])}'/>`).join('')}</div>
+        <div class='sky-main__portrait-movie owl-carousel'>${moviesSpecificCategory.map(movie => `
+          <div class='owl-item__group'>
+            <img src='${(movie['images'][0]['url'])}'/>
+            ${movie['isBlocked'] ? `<i class="fa fa-lock" aria-hidden="true"></i>` : `<i class="fa fa-shopping-cart" aria-hidden="true"></i>`}
+          </div>
+          `).join('')}</div>
       `;
 
       moviesPortraitSection.appendChild(articlePortraitMovies);
     }
-    
-    console.log(moviesPortraitSection);
-
     sliderLoadMovies();
   }
 }
